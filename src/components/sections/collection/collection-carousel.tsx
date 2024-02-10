b78e86337984f7efe67a92830cfdee22019a4f8d
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2024-02-10 18:15:43 +0200

    add pagination to slider

--- src/components/sections/collection/collection-carousel.tsx
+++ src/components/sections/collection/collection-carousel.tsx
@@ -3,13 +3,14 @@
 import React, { useState, useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
-import { Autoplay } from 'swiper/modules';
-import { Navigation } from 'swiper/modules';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import 'swiper/css';
 import 'swiper/css/virtual';
 import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import slider1 from '../../../../public/image/collection-1.png';
 import slider2 from '../../../../public/image/collection-2.png';
 import slider3 from '../../../../public/image/collection-3.png';
@@ -61,45 +62,45 @@ const CollectionCarousel = () => {
 
   return (
     <div>
-      <div>
-        <Swiper
-          initialSlide={0}
-          slidesPerView={1}
-          ref={swiperRef}
-          autoplay={false}
-          // autoplay={{ delay: 5000 }}
-          navigation={{
-            prevEl: '#my-prev-button',
-            nextEl: '#my-next-button',
-          }}
-          // onSlideChange={handleSlideChange}
-          modules={[Navigation]}
-          direction={'horizontal'}
-          autoHeight={true}
-          spaceBetween={40}
-          loop={true}
-          breakpoints={{
-            375: {
-              slidesPerView: 1,
-              slidesPerGroup: 1,
-            },
-            768: {
-              slidesPerView: 1,
-              slidesPerGroup: 1,
-            },
-            1440: {
-              slidesPerView: 2,
-              slidesPerGroup: 2,
-            },
-          }}
-          simulateTouch={true}
-          touchRatio={0.2}
-          effect="slide"
-        >
-          {slides.map((slide, index) => (
-            <SwiperSlide key={index} virtualIndex={index}>
-              <div className={`flex ${slide.bg}`}>
-                <div className="">
+      <Swiper
+        initialSlide={0}
+        slidesPerView={1}
+        // ref={swiperRef}
+        autoplay={false}
+        // autoplay={{ delay: 5000 }}
+        pagination={{ clickable: true }}
+        navigation={{
+          prevEl: '#my-prev-button',
+          nextEl: '#my-next-button',
+        }}
+        // onSlideChange={handleSlideChange}
+        modules={[Navigation, Pagination]}
+        direction={'horizontal'}
+        autoHeight={false}
+        // spaceBetween={40}
+        loop={true}
+        breakpoints={{
+          375: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+          },
+          768: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+          },
+          1440: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+          },
+        }}
+        simulateTouch={true}
+        touchRatio={1}
+        effect="slide"
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index} virtualIndex={index}>
+            <div className={`flex justify-center items-center ${slide.bg} h-[260px] px-4`}>
+              <div className="">
                   <p className="uppercase w-[230px] text-2xl not-italic font-extrabold mt-[18px] mb-4">
                     {slide.title}
                   </p>
@@ -110,27 +111,27 @@ const CollectionCarousel = () => {
                     shop collection
                   </Link>
                 </div>
-                <div className="w-40">
-                  <Image key={index} src={slide.url} alt={slide.title} />
-                </div>
+              <div className="w-40">
+                <Image key={index} src={slide.url} alt={slide.title} />
               </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-      <div className="flex justify-around">
-        <button id="my-prev-button" onClick={() => handlePrevSlide}>
-          prev
-        </button>
-        <button id="my-next-button" onClick={() => handleNextSlide}>
-          next
-        </button>
-      </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
 
 export default CollectionCarousel;
+
+// {/* <div className="flex justify-around">
+//         <button id="my-prev-button" onClick={() => handlePrevSlide}>
+//           prev
+//         </button>
+//         <button id="my-next-button" onClick={() => handleNextSlide}>
+//           next
+//         </button>
+//       </div> */}
 
 // /**
 //  * CollectionCarousel component for nextJS and Tailwind.
