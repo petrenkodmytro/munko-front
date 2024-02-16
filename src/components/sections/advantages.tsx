a1bbd6c00bc2faa44a2d47c4ff17927ece870328
Serhii Khlystun <s.khlistyn@gamil.com>
2024-02-16 16:42:38 +0200

    Add card to section popular

--- src/components/sections/advantages.tsx
+++ src/components/sections/advantages.tsx
@@ -6,20 +6,20 @@ import price from './../../../public/image/advantages-price.png';
 const Advantages = () => {
   return (
     <section className="text-center md:bg-[#F5F5F5] px-4 py-8 md:mx-5 md:rounded md:relative z-10 md:-mt-5 lg:-mt-6 lg:px-20 lg:py-14">
-      <div className="text-2xl font-bold md:text-3xl lg:text-4xl md:font-semibold">
+      <h3 className="text-2xl text-black font-bold md:text-3xl lg:text-4xl md:font-semibold">
         We have figures for everyones&apos;s taste
-      </div>
-      <div className="text-xs font-semibold py-8 px-2 md:px-0 flex justify-evenly md:py-6 md:text-base lg:text-lg md:justify-center">
+      </h3>
+      <div className="text-xs text-black font-semibold py-8 px-2 md:px-0 flex justify-evenly md:py-6 md:text-base lg:text-lg md:justify-center">
         <div className='w-[110px] md:w-auto md:pr-2.5 lg:pr-0'>
-          <img src={original.src} alt="" className="w-10 md:w-16 lg:w-20 m-auto" />
+          <img src={original.src} alt="original" className="w-10 md:w-16 lg:w-20 m-auto" />
           <p className='mt-1.5 pt-1 md:pt-0'>Only Originals</p>
         </div>
         <div className='w-[105px] md:w-auto md:mx-5 lg:mx-20'>
-          <img src={clock.src} alt="" className="w-10 md:w-16 lg:w-20 m-auto" />
+          <img src={clock.src} alt="speed" className="w-10 md:w-16 lg:w-20 m-auto" />
           <p className='mt-1.5'>Fast Worldwide Shipping</p>
         </div>
         <div className='w-[110px] md:w-auto'>
-          <img src={price.src} alt="" className="w-10 md:w-16 lg:w-20 m-auto" />
+          <img src={price.src} alt="price" className="w-10 md:w-16 lg:w-20 m-auto" />
           <p className='mt-1.5 pt-1 md:pt-0'>Affordable Prices</p>
         </div>
       </div>
--- src/components/sections/popular.tsx
+++ src/components/sections/popular.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import icon from './../../../public/image/pinterest-marentorri.png'
 
 const Popular = () => {
-  return <section>Popular</section>;
+
+  return (
+    <section>
+      <div className="text-2xl text-black font-extrabold px-4 mb-5">HOT RIGHT NOW</div>
+      <div>
+        <div className="w-[242px] h-[384px] px-3 py-6 rounded shadow-[0px_0px_20px_0px_rgb(0,0,0,0.15)]">
+          <div className='w-44 h-36 m-auto'>
+            <img src={icon.src} alt="" />
+          </div>
+          <div className='text-base text-black my-5'>
+            <span className=''>POP!</span>
+            <p className='font-bold mb-5'>LOKI:AGENT OF ASGARD - MARVEL</p>
+            <p className='font-bold'>12$</p>
+          </div>
+          <button className='m-auto rounded text-base font-bold py-1.5 hover:py-1 w-full bg-subscribeBtn text-white hover:bg-white hover:text-subscribeBtn hover:border-subscribeBtn hover:border-2'>
+            ADD TO CARD
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Popular;
