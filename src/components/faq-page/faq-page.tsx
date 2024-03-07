384860a09842a8e65b02a2a72924ede8f079c7a7
Serhii Khlystun <s.khlistyn@gamil.com>
2024-03-07 12:49:04 +0200

    Edit faq section and faq page

---
+++ src/components/faq-page/faq-page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import FAQ from '../sections/faq';
+import FaqList from '../sections/faq-list';
+
+type Props = {};
+
+export default function FaqPage(props: Props) {
+  return (
+    <section className="flex flex-col px-4 mt-[25px] mb-[103px] text-xl text-blackCustom font-semibold md:px-5 md:mb-[48px] md:text-2xl lg:mt-[35px]">
+      <h3 className="text-center text-2xl mb-[16px] md:text-[40px] lg:text-[30px] lg:mb-[56px]">FAQ</h3>
+      <FaqList index={12} />
+    </section>
+  );
+}
---
+++ src/components/sections/faq-content.tsx
@@ -0,0 +1,48 @@
+type Props = {
+  element: {
+    question: string;
+    answer: string;
+  };
+};
+
+export default function Questions({ element }: Props) {
+  const handleClick = function (event: React.SyntheticEvent<HTMLElement>) {
+    const parent = event.currentTarget.parentElement as Element;
+    const elem = parent.nextSibling as Element;
+
+    const buttonShow = parent.childNodes[1] as Element;
+    const buttonHide = parent.childNodes[2] as Element;
+    if (elem.className === 'hidden') {
+      elem.className =
+        'text-blackCustom pb-8 font-normal mt-[27px] leading-[35px] md:mt-[4px]';
+      buttonShow.className = 'hidden';
+      buttonHide.className =
+        'text-[65px] font-normal pb-2.5 absolute right-2 top-2/4 -translate-y-2/4';
+    } else {
+      elem.className = 'hidden';
+      buttonShow.className =
+        'text-[65px] font-normal absolute top-2/4 -translate-y-2/4 right-0';
+      buttonHide.className = 'hidden';
+    }
+  };
+
+  return (
+    <>
+      <article className="relative">
+        <h4 className="max-w-[249px] leading-[20px] min-h-[79px] py-4 md:leading-[30px] md:max-w-[548px] lg:max-w-full md:h-full md:py-6">
+          {element.question}
+        </h4>
+        <button
+          onClick={handleClick}
+          className="text-[65px] font-normal absolute top-2/4 -translate-y-2/4 right-0"
+        >
+          +
+        </button>
+        <button onClick={handleClick} className="hidden">
+          -
+        </button>
+      </article>
+      <p className="hidden">{element.answer}</p>
+    </>
+  );
+}
