5b34c05195e6b8660fb33c94db8783a2daa2986b
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2024-03-10 12:59:41 +0200

    add react-loader-spinner

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,22 @@
+'use client';
+
 import React from 'react';
+import { ThreeDots } from 'react-loader-spinner';
 
 type Props = {};
 
-const Loading = (props: Props) => {
-  return <div>Loading...</div>;
-};
-
-export default Loading;
+export default function Spinner({}: Props) {
+  return (
+    <div className="flex justify-center items-center">
+      <ThreeDots
+        height="80"
+        width="80"
+        radius="9"
+        color="#7C9D96"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        visible={true}
+      />
+    </div>
+  );
+}
