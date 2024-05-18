6d7e9a8680ea5a4aefd416d95120ec0f3cbd0710
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2024-05-18 16:18:17 +0300

    fixed review list

--- src/components/product-card/reviewItem.tsx
+++ src/components/product-card/reviewItem.tsx
@@ -13,7 +13,7 @@ type Props = {
 
 const ReviewItem = ({ reviwe }: Props) => {
   const [isShowMore, setIsShowMore] = useState(false);
-
+  // console.log(reviwe);
   const toggleReadMoreLess = () => {
     setIsShowMore(!isShowMore);
   };
@@ -22,7 +22,7 @@ const ReviewItem = ({ reviwe }: Props) => {
       <div className="flex flex-shrink-0 justify-center items-center w-10 h-10 rounded-[50%] bg-[#D9D9D9]">
         <ReviewIcon />{' '}
       </div>
-      <div>
+      <div className="w-full">
         <div className="flex justify-between">
           <p className="text-base font-semibold">{reviwe.username}</p>
           <Rating
@@ -35,15 +35,19 @@ const ReviewItem = ({ reviwe }: Props) => {
         </div>
         <div className="">
           {!isShowMore && (
-            <p className="text-xs font-medium">{reviwe.review.slice(0, 230)}...</p>
+            <p className="text-xs font-medium">
+              {reviwe.review.slice(0, 230)}...
+            </p>
           )}
           {isShowMore && <p className="text-xs font-medium">{reviwe.review}</p>}
-          <button
-            onClick={toggleReadMoreLess}
-            className="underline text-xs font-semibold"
-          >
-            {isShowMore ? 'Read less' : 'Read more'}
-          </button>
+          {reviwe.review.length > 230 && (
+            <button
+              onClick={toggleReadMoreLess}
+              className="underline text-xs font-semibold"
+            >
+              {isShowMore ? 'Read less' : 'Read more'}
+            </button>
+          )}
         </div>
       </div>
     </>
