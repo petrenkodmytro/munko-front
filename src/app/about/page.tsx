d54852635b665d6941f671d6235824653fc45cbc
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2024-08-14 16:25:41 +0300

    add content and bg-img

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,8 +6,16 @@ const [aboutBaner, about1, about2, about3, about4] = aboutUs;
 
 const About = () => {
   return (
-    <section className="text-[#08080D] px-4 pt-6 pb-10 md:px-5 md:pb-[72px] xl:px-20 xl:pt-9">
-      <Image src={aboutBaner} alt="image munko" loading="lazy" />
+    <section className="text-[#08080D]   pb-10  md:pb-[72px] xl:px-20 xl:pt-9">
+      <div
+        className="w-full h-[152px] relative   bg-contain bg-no-repeat"
+        style={{
+          backgroundImage: `url('./image/about-banner.png')`,
+        }}
+      >
+        {/* <div className="relative top-0 left-0 w-full h-full z-10 bg-black/50 flex items-center justify-center flex-col gap-5 "></div> */}
+      </div>
+      {/* <Image src={aboutBaner} alt="image munko" loading="lazy" /> */}
 
       <div>
         <p className="text-2xl font-semibold text-center mb-6 md:text-3xl">
@@ -48,10 +56,24 @@ const About = () => {
       </div>
 
       <div>
-      <Image src={about3} alt="image munko" loading="lazy" />
-      <p>Join our community, where everyone can find their favorite hero and share their passion. We are sure that you will find here not only figurines, but also new friends and an inexhaustible source of inspiration.</p>
-      <p>Welcome to our magical place where everyone can make their little world fantastic!</p>
-      <Image src={about4} alt="image munko" loading="lazy" />
+        <Image src={about3} alt="image munko" loading="lazy" />
+        <p>
+          Join our community, where everyone can find their favorite hero and
+          share their passion. We are sure that you will find here not only
+          figurines, but also new friends and an inexhaustible source of
+          inspiration.
+        </p>
+        <p>
+          Welcome to our magical place where everyone can make their little
+          world fantastic!
+        </p>
+        <Image src={about4} alt="image munko" loading="lazy" />
+        <Link
+          href={'/catalog'}
+          className="mx-14 h-[46px] rounded px-8 text-xl font-semibold bg-subscribeBtn text-white hover:bg-white hover:text-subscribeBtn hover:border-subscribeBtn hover:border-2 md:block md:m-auto duration-200 ease-linear"
+        >
+          CATALOG
+        </Link>
       </div>
     </section>
   );
