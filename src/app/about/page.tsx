5e80590d9346de09b9109cf896e4febf4289b792
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2024-08-15 11:53:23 +0300

    add style about us tablet

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,16 +6,16 @@ const [aboutBaner, about1, about2, about3, about4] = aboutUs;
 
 const About = () => {
   return (
-    <section className="text-[#08080D]   pb-10  md:pb-[72px] xl:px-20 xl:pt-9 text-base font-medium">
+    <section className="text-[#08080D]   pb-10  md:pb-[72px] xl:px-20 xl:pt-9 text-base md:text-xl font-medium">
       <div
-        className="w-full h-[152px] relative bg-contain bg-no-repeat"
+        className="w-full h-[152px] md:h-[278px] bg-contain bg-no-repeat"
         style={{
           backgroundImage: `url('./image/about-banner.png')`,
         }}
       ></div>
 
-      <div className="relative max-w-[358px] mx-auto mt-[-20px] rounded-md p-5 bg-lightGrey shadow-[0px_0px_30px_0px_rgb(0,0,0,0.15)]">
-        <p className="text-2xl font-semibold text-center mb-5 md:text-3xl">
+      <div className="relative w-[358px] md:w-[680px] mx-auto mt-[-20px] rounded-md p-5 bg-lightGrey shadow-[0px_0px_30px_0px_rgb(0,0,0,0.15)]">
+        <p className="text-2xl font-semibold text-center mb-5 md:text-4xl">
           About us
         </p>{' '}
         <p>
@@ -29,7 +29,7 @@ const About = () => {
           army of favorite characters. And now we are here, opening this store,
           to share our passion with you.
         </p>
-        <p>
+        <p className="pt-4">
           What makes us special? We are not only sellers, but also true
           supporters of the culture we represent. Each element of our assortment
           is chosen with love and care, because we believe that every fan
@@ -37,13 +37,13 @@ const About = () => {
         </p>
       </div>
 
-      <div className="overflow-hidden flex gap-3 pb-7 mt-[-10px]">
-        <div className="ml-[-35px] shrink-0">
+      <div className="overflow-hidden flex gap-3 md:gap-8 pb-7 mt-[-10px]">
+        <div className="ml-[-35px] md:ml-[-15px] shrink-0">
           <Image
             src={about1}
             alt="image munko"
             loading="lazy"
-            className="h-[295px] w-auto rounded"
+            className="h-[295px] md:h-[336px] w-auto rounded"
           />
         </div>
         <div className="">
@@ -51,9 +51,9 @@ const About = () => {
             src={about2}
             alt="image munko"
             loading="lazy"
-            className="w-[221px] rounded-l"
+            className="w-[221px] md:w-[458px] md:ml-auto rounded-l"
           />
-          <p className="pt-4 pr-4 leading-5">
+          <p className="pt-4 md:pt-10 pr-4 md:pr-5 leading-5 md:leading-7">
             We want to create not just a store, but a real community for
             everyone who, like us, is devoted to their favorite characters.
             Whether you are a beginner collector or a seasoned geek, we want to
@@ -62,28 +62,30 @@ const About = () => {
         </div>
       </div>
 
-      <div className=" relative bg-lightGrey shadow-[0px_0px_7px_0px_rgb(0,0,0,0.3)]">
-        <div className="w-[150px]  [clip-path:circle(100.0%_at_0_50%)]">
+      <div className="relative py-2 md:py-5 bg-lightGrey shadow-[0px_0px_27px_0px_rgb(0,0,0,0.15)]">
+        <div className="w-[150px] md:w-[236px] [clip-path:circle(100.0%_at_0_50%)]">
           <Image src={about3} alt="image munko" loading="lazy" />
         </div>
-        <p className="px-5 py-2">
-          Join our community, where everyone can find their favorite hero and
-          share their passion. We are sure that you will find here not only
-          figurines, but also new friends and an inexhaustible source of
-          inspiration.
-        </p>
+        <div className="px-5 py-2 md:pt-5 md:absolute top-0 left-[230px]">
+          <p className="mb-3">
+            Join our community, where everyone can find their favorite hero and
+            share their passion. We are sure that you will find here not only
+            figurines, but also new friends and an inexhaustible source of
+            inspiration.
+          </p>
+          <p className="">
+            Welcome to our magical place where everyone can make their little
+            world fantastic!
+          </p>
+        </div>
 
-        <p className="px-5 py-2">
-          Welcome to our magical place where everyone can make their little
-          world fantastic!
-        </p>
-        <div className="w-[150px] ml-auto [clip-path:circle(100%_at_100%_50%)]">
+        <div className="w-[150px] md:w-[236px] ml-auto [clip-path:circle(100%_at_100%_50%)]">
           <Image src={about4} alt="image munko" loading="lazy" />
         </div>
 
         <Link
           href={'/catalog'}
-          className="inline-block absolute bottom-12 left-10   rounded px-8 py-2 text-xl font-semibold bg-subscribeBtn text-white hover:bg-white hover:text-subscribeBtn hover:border-subscribeBtn hover:border-2 md:block md:m-auto duration-200 ease-linear"
+          className="inline-block absolute bottom-12 md:bottom-24 left-10 md:left-52 rounded px-8 py-2 text-xl font-semibold bg-subscribeBtn text-white hover:bg-white hover:text-subscribeBtn hover:border-subscribeBtn hover:border-2 md:block md:m-auto duration-200 ease-linear"
         >
           CATALOG
         </Link>
